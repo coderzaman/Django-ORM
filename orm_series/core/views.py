fc1cf4eedcdb07dcd8a1d9678eebe58b545bb81d
coderzaman <coder.aktar@gmail.com>
2025-03-17 22:26:19 +0600

    Query Optimization with Prefetch_related and select_related

--- orm_series/core/views.py
+++ orm_series/core/views.py
@@ -2,9 +2,27 @@ from django.shortcuts import render
 from .models import Sale, Rating, Restaurant
 # Create your views here.
 
+# def home(request):
+#    restaurants = Restaurant.objects.prefetch_related('ratings',  'sales')
+#    context = {'restaurants':restaurants}
+    
+#    return render(request, 'home.html', context)   
+
+# def home(request):
+#    ratings = Rating.objects.only('rating','restaurant__name', 'user__username').select_related('restaurant','user')
+#    context = {'ratings':ratings}
+    
+#    return render(request, 'home.html', context)   
+
+
+# def home(request):
+#    ratings = Rating.objects.only('rating','restaurant__name','user__username', 'user__username').select_related('restaurant','user')
+#    context = {'ratings':ratings}
+    
+#    return render(request, 'home.html', context)   
+
 def home(request):
-   restaurants = Restaurant.objects.all()
-   context = {'restaurants':restaurants}
+   ratings = Rating.objects.defer('restaurant__name').select_related('restaurant')
+   context = {'ratings':ratings}
     
    return render(request, 'home.html', context)   
-
